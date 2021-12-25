5609fd3b5eea96df7d7fd27931d940acc0736afd
고준서 <sta06167@naver.com>
2021-12-25 22:26:52 +0900

    53분 25초까지

---
+++ App.js
@@ -0,0 +1,17 @@
+// import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import {Button, StyleSheet, Text, View } from 'react-native';
+import tw from "tailwind-rn"
+import { AuthProvider } from "./hooks/useAuth";
+import StackNavigator from "./StackNavigator";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <StackNavigator/>
+      </AuthProvider>
+    </NavigationContainer>
+  );
+}
---
+++ hooks/useAuth.js
@@ -0,0 +1,19 @@
+
+import { createContext, useContext } from 'react';
+import { View, Text } from 'react-native'
+
+const AuthContext = createContext({})
+
+export const AuthProvider = ({children}) => {
+    return (
+        <AuthContext.Provider value={
+            {user:"Sunny"}
+        }>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default function useAuth() {
+   return useContext(AuthContext); 
+}
